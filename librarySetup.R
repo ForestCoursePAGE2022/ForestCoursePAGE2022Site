647323c6dd8b04725644d9cab5b085653e9afb8a
ricodan <>
2022-05-17 15:28:54 +0200

    first set of files

---
+++ librarySetup.R
@@ -0,0 +1,41 @@
+library(devtools)
+
+
+#Creating the file paths
+extension <- character(0)
+pkg_folder <- character(0)
+
+switch(Sys.info()[['sysname']],
+       Windows= {extension<-(".zip")
+                pkg_folder <-("Windows")}, #WINDOWS
+       Linux  = {extension<-(".tar.gz")
+                pkg_folder <-("Linux")
+                print("No support implemented for Linux yet")}, #LINUX
+       Darwin = {extension<-(".tgz")
+                pkg_folder <-("Mac")}) #MAC
+
+stem_path <- paste0(getwd(), "/coursepackages/", pkg_folder,"/")
+
+pkgs <- c("tidyverse_1.2.1", "table1_1.1", 
+          "stringr_1.4.0", "MASS_7.3-51.4", 
+          "ggpubr_0.2.3", "foreach_1.4.7", 
+          "doParallel_1.0.15")
+
+course_lib_path <- paste0(getwd(), "/coursepackages")
+all_pkgs <- paste0(stem_path, pkgs, extension)
+
+#Install the packages
+options(install.packages.check.source = "no")
+install.packages(pkgs=all_pkgs, lib=course_lib_path, repos = NULL, type = "binary")
+
+#Link the libraries
+library("tidyverse", lib.loc = installed_pkg_path)
+library("table1", lib.loc = installed_pkg_path)
+library("stringr", lib.loc = installed_pkg_path)
+library("MASS", lib.loc = installed_pkg_path)
+library("ggpubr", lib.loc = installed_pkg_path)
+library("foreach", lib.loc = installed_pkg_path)
+library("doParallel", lib.loc = installed_pkg_path)
+
+#Tests
+
